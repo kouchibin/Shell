6df9a83fc7b53c0b16af6542aff67be9fdf3674e
Kou Chibin <kouchibin@gmail.com>
2018-09-18 18:34:23 +0200

    first commit

---
+++ lsh.c
@@ -0,0 +1,178 @@
+/* 
+ * Main source code file for lsh shell program
+ *
+ * You are free to add functions to this file.
+ * If you want to add functions in a separate file 
+ * you will need to modify Makefile to compile
+ * your additional functions.
+ *
+ * Add appropriate comments in your code to make it
+ * easier for us while grading your assignment.
+ *
+ * Submit the entire lab1 folder as a tar archive (.tgz).
+ * Command to create submission archive: 
+      $> tar cvf lab1.tgz lab1/
+ *
+ * All the best 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "parse.h"
+
+/*
+ * Function declarations
+ */
+
+void PrintCommand(int, Command *);
+void PrintPgm(Pgm *);
+void stripwhite(char *);
+pid_t wait(int *wstatus);
+pid_t fork(void);
+
+/* When non-zero, this global means the user is done using this program. */
+int done = 0;
+
+/*
+ * Name: main
+ *
+ * Description: Gets the ball rolling...
+ *
+ */
+int main(void)
+{
+  Command cmd;
+  int n;
+
+  while (!done) {
+
+    char *line;
+    line = readline("> ");
+
+    if (!line) {
+      /* Encountered EOF at top level */
+      done = 1;
+    }
+    else {
+      /*
+       * Remove leading and trailing whitespace from the line
+       * Then, if there is anything left, add it to the history list
+       * and execute it.
+       */
+      stripwhite(line);
+
+      if(*line) {
+        add_history(line);
+        /* execute it */
+        n = parse(line, &cmd);
+        if (n == 1) {
+          // parse cmd successfully
+          pid_t pid = fork();
+          if (pid < 0) {
+            printf("Error when forking...\n");
+            exit(-1);
+          } else if (pid == 0) {
+            // in child process
+            printf("arg: %s\n",cmd.pgm->pgmlist[0]);
+            execvp(cmd.pgm->pgmlist[0], cmd.pgm->pgmlist);
+            // on error
+            printf("Error when executing cmd...\n");
+            exit(-1);
+            
+          } else {
+            // in parent process
+            int status;
+            if (wait(&status) == -1) {
+              printf("Error when waiting for child process to complete...\n");
+              exit(-1);
+            }
+          }
+        } else {
+          // parse cmd fail
+          printf("Parsing cmd failed...\n");
+        }
+        //PrintCommand(n, &cmd);
+      }
+    }
+    
+    if(line) {
+      free(line);
+    }
+  }
+  return 0;
+}
+
+/*
+ * Name: PrintCommand
+ *
+ * Description: Prints a Command structure as returned by parse on stdout.
+ *
+ */
+void
+PrintCommand (int n, Command *cmd)
+{
+  printf("Parse returned %d:\n", n);
+  printf("   stdin : %s\n", cmd->rstdin  ? cmd->rstdin  : "<none>" );
+  printf("   stdout: %s\n", cmd->rstdout ? cmd->rstdout : "<none>" );
+  printf("   bg    : %s\n", cmd->bakground ? "yes" : "no");
+  PrintPgm(cmd->pgm);
+}
+
+/*
+ * Name: PrintPgm
+ *
+ * Description: Prints a list of Pgm:s
+ *
+ */
+void
+PrintPgm (Pgm *p)
+{
+  if (p == NULL) {
+    return;
+  }
+  else {
+    char **pl = p->pgmlist;
+
+    /* The list is in reversed order so print
+     * it reversed to get right
+     */
+    PrintPgm(p->next);
+    printf("    [");
+    while (*pl) {
+      printf("%s ", *pl++);
+    }
+    printf("]\n");
+  }
+}
+
+/*
+ * Name: stripwhite
+ *
+ * Description: Strip whitespace from the start and end of STRING.
+ */
+void
+stripwhite (char *string)
+{
+  register int i = 0;
+
+  while (isspace( string[i] )) {
+    i++;
+  }
+  
+  if (i) {
+    strcpy (string, string + i);
+  }
+
+  i = strlen( string ) - 1;
+  while (i> 0 && isspace (string[i])) {
+    i--;
+  }
+
+  string [++i] = '\0';
+}
