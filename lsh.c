542653ca54a244a4a6e5ce5d2c3870b6671bb4c4
Kou Chibin <kouchibin@gmail.com>
2018-09-30 12:42:06 +0200

    finshed all mandatory features

--- lsh.c
+++ lsh.c
@@ -1,8 +1,8 @@
-/* 
+/*
  * Main source code file for lsh shell program
  *
  * You are free to add functions to this file.
- * If you want to add functions in a separate file 
+ * If you want to add functions in a separate file
  * you will need to modify Makefile to compile
  * your additional functions.
  *
@@ -10,10 +10,10 @@
  * easier for us while grading your assignment.
  *
  * Submit the entire lab1 folder as a tar archive (.tgz).
- * Command to create submission archive: 
+ * Command to create submission archive:
       $> tar cvf lab1.tgz lab1/
  *
- * All the best 
+ * All the best
  */
 
 
@@ -30,6 +30,8 @@
 
 #include "parse.h"
 
+#define PERM S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+
 /*
  * Function declarations
  */
@@ -40,13 +42,17 @@ void stripwhite(char *);
 pid_t wait(int *wstatus);
 pid_t fork(void);
 int setpgid(pid_t pid, pid_t pgid);
-void signal_handler(int signo);
 void executePgm(Pgm *pgm);
 void eexit(char *description);
 void setupExecEnv(Command *cmd);
+void SIGCHLD_handler(int signo);
+void SIGINT_handler(int signo);
 
 /* When non-zero, this global means the user is done using this program. */
 int done = 0;
+
+/* The pid of the process running in foreground. */
+pid_t foreground_pid = 0;
 
 /*
  * Name: main
@@ -62,12 +68,13 @@ int main(void)
   while (!done) {
 
     char *line;
-    
-    /* register singal_handler for SIGCHLD */
-    signal(SIGCHLD, signal_handler);
-    
+
+    /* Register singal handlers */
+    signal(SIGCHLD, SIGCHLD_handler);
+    signal(SIGINT, SIGINT_handler);
+
     line = readline("> ");
-    
+
     if (!line) {
       /* Encountered EOF at top level */
       done = 1;
@@ -84,9 +91,24 @@ int main(void)
         add_history(line);
 
         n = parse(line, &cmd);
-        
+
         if (n == 1) {
           /* Parse cmd successfully */
+
+          /* Handle the cd command */
+          if (!strcmp((cmd.pgm) -> pgmlist[0], "cd") && !((cmd.pgm) -> next)) {
+            char *to = (cmd.pgm) -> pgmlist[1];
+            if (!to)
+              to = getenv("HOME");
+            if (chdir(to) == -1)
+              perror("chdir error: ");
+            continue;
+          }
+
+          /* Handle the exit command */
+          if (!strcmp((cmd.pgm) -> pgmlist[0], "exit")&& !((cmd.pgm) -> next))
+            exit(0);
+
           pid_t pid = fork();
           if (pid < 0) {
             eexit("Error when forking.\n");
@@ -99,22 +121,27 @@ int main(void)
           } else {
             /* In parent process */
             int status;
+
+            /* Record the child process's pid, in case we want to kill it */
+            foreground_pid = pid;
+
             if (!cmd.bakground && wait(&status) == -1) {
-              /* Wait for child process to finish and clean up. 
+              /* Wait for child process to finish and clean up.
                * For background process, leave it to signal_handler.
                */
               eexit("Error when waiting for child process to complete.\n");
-            } 
+            }
+
+            /* Nothing in foreground now */
+            foreground_pid = 0;
           }
         } else {
           printf("Parsing cmd failed.\n");
         }
-        
+
       }
     }
 
-    
-    
     if(line) {
       free(line);
     }
@@ -125,9 +152,9 @@ int main(void)
 /*
  * Name: setupExecEnv
  *
- * Description: Setup Execution Environment. Including wether or not runing in 
+ * Description: Setup Execution Environment. Including wether or not runing in
  * background and redirection.
- * 
+ *
  */
 void setupExecEnv(Command *cmd)
 {
@@ -135,7 +162,7 @@ void setupExecEnv(Command *cmd)
     /* Run command in background */
     setpgid(0, 0);
   }
-  
+
   if (cmd -> rstdin) {
     /* Redirect stdin to file */
     int infd;
@@ -150,7 +177,7 @@ void setupExecEnv(Command *cmd)
   if (cmd -> rstdout) {
     /* Redirect stdout to file */
     int outfd;
-    if ((outfd = open(cmd -> rstdout, O_CREAT | O_WRONLY | O_TRUNC)) < 0)
+    if ((outfd = open(cmd -> rstdout, O_CREAT | O_WRONLY | O_TRUNC, PERM)) < 0)
       eexit("open file error.\n");
     if (outfd != STDOUT_FILENO)
       if (dup2(outfd, STDOUT_FILENO) != STDOUT_FILENO)
@@ -169,12 +196,12 @@ void setupExecEnv(Command *cmd)
  */
 void executePgm(Pgm *pgm) {
   if (pgm == NULL) return;
-  
+
   if (pgm -> next) {
     /* Has more command */
     int pipefd[2], status;
     pid_t pid;
-    if (pipe(pipefd) == -1) 
+    if (pipe(pipefd) == -1)
       eexit("Fail to create pipe.\n");
     pid = fork();
     if (pid < 0) {
@@ -201,14 +228,20 @@ void executePgm(Pgm *pgm) {
           eexit("dup2 error.\n");
         close(pipefd[0]);
       }
-      
+
+      /* Wait for the child process to finish */
+      wait(&status);
+
+      /* Execute the expected program after child finish*/
       execvp(pgm->pgmlist[0], pgm->pgmlist);
-      perror("lsh error: ");
+      perror("lsh error");
+      exit(-1);
     }
   } else {
     /* No more command */
     execvp(pgm->pgmlist[0], pgm->pgmlist);
     perror("lsh error");
+    exit(-1);
   }
 }
 
@@ -225,12 +258,12 @@ void eexit(char *description)
 }
 
 /*
- * Name: signal_handler
+ * Name: SIGCHLD_handler
  *
- * Description: Handle the SIGCHLD signal and clean up the zombies 
+ * Description: Handle the SIGCHLD signal and clean up the zombies
  *
  */
-void signal_handler(int signo) 
+void SIGCHLD_handler(int signo)
 {
   if (signo == SIGCHLD) {
     int status;
@@ -240,6 +273,25 @@ void signal_handler(int signo)
     }
   }
 }
+
+/*
+ * Name: SIGINT_handler
+ *
+ * Description: Handle the SIGINT signal and terminate foreground running
+ * process.
+ *
+ */
+void SIGINT_handler(int signo)
+{
+  if (signo == SIGINT && foreground_pid > 0) {
+    if ((kill(foreground_pid, SIGTERM)) == -1)
+      eexit("Error when terminating foreground process.\n");
+    else
+      foreground_pid = 0;
+  }
+
+}
+
 
 /*
  * Name: PrintCommand
@@ -299,7 +351,7 @@ stripwhite (char *string)
   while (isspace( string[i] )) {
     i++;
   }
-  
+
   if (i) {
     strcpy (string, string + i);
   }
